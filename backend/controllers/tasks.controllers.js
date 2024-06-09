dccf23aa677e5a8b16867b6201b4c299a8289647
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-06-09 17:14:58 -0500

    controladores

---
+++ backend/controllers/tasks.controllers.js
@@ -0,0 +1,15 @@
+export const getTasks = (req, res) => {
+    res.send('Obteniendo tareas')
+}
+export const getTask = (req, res) => {
+    res.send('Obteniendo un tarea')
+}
+export const createTasks = (req, res) => {
+    res.send('Creando tarea')
+}
+export const updateTasks = (req, res) => {
+    res.send('Actualizando tareas')
+}
+export const deleteTasks = (req, res) => {
+    res.send('Eliminando tareas')
+}
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import {PORT} from './config.js'
 //imp router
 import indexRoutes from './routes/index.routes.js'
+import tasksRoutes from './routes/tasks.routes.js'
 
 const app = express()
 
 app.use(indexRoutes)
+app.use(tasksRoutes)
 
 app.listen(PORT)
 console.log(`Server is running port ${PORT}`)
